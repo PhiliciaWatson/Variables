2b592d12c218085baacbd0d821854693f8822765
PhiliciaWatson <philiciawatson7@gmail.com>
2020-07-07 05:45:08 -0400

    Submit

---
+++ js/app.js
@@ -0,0 +1,35 @@
+//declarations
+var item1=1
+var item2=2
+var item3=3
+var item4=4
+var item5=5
+var item6=6
+var item7=7
+var item8=8
+var item9=9
+var item10=10
+//equations
+item1 +=2;
+item10 +=7;
+item3 -=15;
+item7 -=5;
+item9 /=3;
+item6 /=2;
+item4 *=7;
+item5 *=4;
+item2 ++;
+item8 --;
+//consoles
+console.log(item1);
+console.log(item10);
+console.log(item3);
+console.log(item7);
+console.log(item9);
+console.log(item6);
+console.log(item4);
+console.log(item5);
+console.log(item2++);
+console.log(item2);
+console.log(item8--);
+console.log(item8);
